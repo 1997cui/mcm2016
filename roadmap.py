22904e6f60fb6a04b6dbbc1f3e713fab23bb1dff
1997cui <1997cui@sina.com>
2016-09-10 11:48:59 +0800

    Finish enqueue and dequeue

--- roadmap.py
+++ roadmap.py
@@ -1,24 +1,51 @@
 class Road:
-    def __init__(self, src=0, dst=0, crowd=0, length=1000, speed=10):
+    def __init__(self, src=0, dst=0, width=0, length=1000, speed=10):
         self.next = -1
         self.src = src
         self.dst = dst
-        self.crowd = crowd
+        self.width = width
         self.length = length
         self.speed = speed
         self.car_num = 0
-        self.queue = [[], ]
+        self.queues = []
+
+    def enqueue(self, next_road_index, car):
+        for queue in self.queues:
+            if queue[0] == next_road_index:
+                queue[1].append(car)
+
+    def dequeue(self, next_road_index):
+        for queue in self.queues:
+            if queue[0] == next_road_index:
+                return queue[1].pop(0)
+
 
 
 class Map:
     def __init__(self, n=10):
-        self.__roads__ = [None] * n
-        self.__crosses__ = [None] * n
-        self.__edge__ = []
+        self.__fist_road_of_cross = [None] * n
+        self.__crosses = [None] * n
+        self.roads = []
+        self.n = n
+        self.changeable = True
         for i in range(0, n):
             pass
 
-    def add_road(self, src=0, dst=0, crowd=0, length=1000, speed=10):
-        self.__edge__.append(Road(src, dst, crowd, length, speed))
-        self.__edge__[-1].next = self.__roads__[src]
-        self.__roads__[src] = len(self.__edge__) - 1
+    def add_road(self, src=0, dst=0, width=0, length=1000, speed=10):
+        if not self.changeable:
+            raise AttributeError("You could not change roads now!")
+        self.roads.append(Road(src, dst, width, length, speed))
+        self.roads[-1].next = self.__fist_road_of_cross[src]
+        self.__fist_road_of_cross[src] = len(self.roads) - 1
+
+    def init_queue(self):
+        self.changeable = False
+        for i in range(0, self.n):
+            j = self.__fist_road_of_cross[i]
+            while j != -1:
+                dst = self.roads[j].dst
+                k = self.__fist_road_of_cross[dst]
+                while k != -1:
+                    self.roads[j].queue.append((k, [], 1))
+                    k = self.roads[k].next
+                j = self.roads[j].next
