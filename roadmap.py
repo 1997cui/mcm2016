262e2f074c6f4470eff2e814d91b9590f2b902f6
1997cui <1997cui@sina.com>
2016-09-10 11:00:52 +0800

    邻接表用Index寻址，而不是指针

--- roadmap.py
+++ roadmap.py
@@ -1,23 +1,24 @@
 class Road:
     def __init__(self, src=0, dst=0, crowd=0, length=1000, speed=10):
-        self.next = None
+        self.next = -1
         self.src = src
         self.dst = dst
         self.crowd = crowd
         self.length = length
         self.speed = speed
         self.car_num = 0
-        self.queue = []
+        self.queue = [[], ]
 
-    def enqueue(self, ):
+
 class Map:
     def __init__(self, n=10):
-        self.roads = [None] * n
-        self.crosses = [None] * n
+        self.__roads__ = [None] * n
+        self.__crosses__ = [None] * n
+        self.__edge__ = []
         for i in range(0, n):
             pass
 
     def add_road(self, src=0, dst=0, crowd=0, length=1000, speed=10):
-        t = Road(src, dst, crowd, length, speed)
-        t.next = self.roads[src]
-        self.roads[src] = t
+        self.__edge__.append(Road(src, dst, crowd, length, speed))
+        self.__edge__[-1].next = self.__roads__[src]
+        self.__roads__[src] = len(self.__edge__) - 1
