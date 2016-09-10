11c148d0c5270b76b4f778dd366462ecf1feab60
Russell-yy <418833586@qq.com>
2016-09-10 10:49:07 +0800

    Update car.py

--- car.py
+++ car.py
@@ -1,9 +1,9 @@
 from misc.py import findnextroad
 class Car:
-	def __init__(self,startime_,car_index,destination,currentroad,currentqueue=0,drivingdist=0):
+	def __init__(self,startime_,drivingtime=0,car_index,destination,currentroad,currentqueue=0,drivingdist=0):
 		self.startime=startime
 		#startime is the time when the car starts to drive 
-		self.drivingtime
+		self.drivingtime=drivingtime
 		#drivingtime is the amount of time the car has driven for
 		self.destination=destination
 		self.nextcross=findnextroad()
