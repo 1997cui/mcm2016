814d1780c84f96f285fb96e5c1767b206e8c9225
trumpet <zh19970205@126.com>
2016-09-11 16:36:56 +0800

    format

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ def main():
        {10:30,9:15,13:30},
        {9:60,10:30,13:30},
        {12:30,1:15}], 3.0, 4.3)
-    for i in range(100):
+    for i in range(50):
         city_map.cars.append(Car(i, i, 2, 0))
     for i in city_map.cars:
         i()
